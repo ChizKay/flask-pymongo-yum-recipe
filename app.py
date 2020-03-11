1c79525010c6593f3f0c0a112c2eefd0c2093e6d
Ubuntu <ubuntu@ip-172-31-85-92.ec2.internal>
2020-03-11 19:50:47 +0000

    add recipe functionality

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_recipe')
 def get_recipe():
-    return render_template("recipes.html", recipes=mongo.db.recipes.find(), cuisines=mongo.db.cuisines.find())
+    return render_template("recipes.html")
     
 
 @app.route('/add_recipe')
@@ -26,9 +26,70 @@ def add_recipe():
 
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
-    tasks = mongo.db.tasks
-    tasks.insert_one(request.form.to_dict())
-    return redirect(url_for('get_recipe'))    
+    recipes = mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('my_recipe'))    
+
+@app.route('/update_cuisine', methods=["POST"])
+def update_cuisine():
+    cuisines = mongo.db.cuisines
+    cuisines.update(
+    {
+        'cuisine_name':request.form.get('cuisine_name')
+    })
+    return redirect(url_for('get_recipe'))   
+    
+@app.route('/update_french', methods=["POST"])
+def update_french():
+    french = mongo.db.french_cuisine
+    french.update(
+    {
+        'dish_name':request.form.get('dish_name'),
+        'image':request.form.get('image'),
+        'ingredients':request.form.get('ingredients'),
+        'instructions':request.form.get('instructions'),
+    })
+    return redirect(url_for('french_cuisine'))  
+    
+    
+@app.route('/update_italian', methods=["POST"])
+def update_italian():
+    italian = mongo.db.italian_cuisine
+    italian.update(
+    {
+        'dish_name':request.form.get('dish_name'),
+        'image':request.form.get('image'),
+        'ingredients':request.form.get('ingredients'),
+        'instructions':request.form.get('instructions'),
+    })
+    return redirect(url_for('italian_cuisine'))
+
+
+@app.route('/update_mexican', methods=["POST"])
+def update_mexican():
+    mexican = mongo.db.mexican_cuisine
+    mexican.update(
+    {
+        'dish_name':request.form.get('dish_name'),
+        'image':request.form.get('image'),
+        'ingredients':request.form.get('ingredients'),
+        'instructions':request.form.get('instructions'),
+    })
+    return redirect(url_for('mexican_cuisine'))   
+    
+
+@app.route('/update_other', methods=["POST"])
+def update_other():
+    other = mongo.db.other
+    other.update(
+    {
+        'dish_name':request.form.get('dish_name'),
+        'image':request.form.get('image'),
+        'ingredients':request.form.get('ingredients'),
+        'instructions':request.form.get('instructions'),
+    })
+    return redirect(url_for('other_cuisine'))
+
     
     
 @app.route('/french_cuisine')
@@ -49,6 +110,11 @@ def mexican_cuisine():
 @app.route('/other_cuisine')
 def other_cuisine():
     return render_template("other.html", other=mongo.db.other.find())
+    
+
+@app.route('/my_recipe')
+def my_recipe():
+    return render_template("myrecipe.html", recipes=mongo.db.recipes.find())
     
     
 if __name__ == '__main__':
